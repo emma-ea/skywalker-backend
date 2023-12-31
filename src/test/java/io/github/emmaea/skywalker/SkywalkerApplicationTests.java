7d2ecb2514dae6688e31df7717d6b40c4cab0e2b
Emmanuel Awuah <emmanuelawuah.dev@gmail.com>
2023-12-31 13:38:02 +0000

    initial commit

---
+++ src/test/java/io/github/emmaea/skywalker/SkywalkerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.emmaea.skywalker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkywalkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
